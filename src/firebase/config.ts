e0ec7bcee1ce3fe145c72ba33e94206f0001b026
KAWOOYA RAYMOND <kawooyaraymond40@gmail.com>
2025-10-23 18:02:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-559195456-fff9f",
+  "appId": "1:981254510003:web:30852956cb1109586fe7c6",
+  "apiKey": "AIzaSyAA_qQB5UhLVDUbBFfQrqIlopmYKpeEeHI",
+  "authDomain": "studio-559195456-fff9f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "981254510003"
+};
