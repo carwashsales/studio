479f906f3facf4f08aeb77ec6dfa8d27de0e9616
KAWOOYA RAYMOND <kawooyaraymond40@gmail.com>
2025-10-24 11:46:24 +0000

    391-921c10a0167837fc.js:1 Automatic initialization failed. Falling back

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -7,29 +7,26 @@ import { getFirestore } from 'firebase/firestore'
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  if (!getApps().length) {
-    // Important! initializeApp() is called without any arguments because Firebase App Hosting
-    // integrates with the initializeApp() function to provide the environment variables needed to
-    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
-    // without arguments.
-    let firebaseApp;
-    try {
-      // Attempt to initialize via Firebase App Hosting environment variables
-      firebaseApp = initializeApp();
-    } catch (e) {
-      // Only warn in production because it's normal to use the firebaseConfig to initialize
-      // during development
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
-
-    return getSdks(firebaseApp);
+  if (getApps().length > 0) {
+    return getSdks(getApp());
   }
 
-  // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
+  // When deployed to Firebase App Hosting, the FIREBASE_CONFIG environment
+  // variable is automatically populated.
+  // Vercel and other platforms will not have this variable, so we fall back
+  // to the firebaseConfig object.
+  if (process.env.NEXT_PUBLIC_FIREBASE_CONFIG) {
+    try {
+      const app = initializeApp(JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG));
+      return getSdks(app);
+    } catch (e) {
+      console.error("Failed to parse NEXT_PUBLIC_FIREBASE_CONFIG", e);
+    }
+  }
+  
+  // Fallback for local development and other hosting providers
+  const app = initializeApp(firebaseConfig);
+  return getSdks(app);
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
