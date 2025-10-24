3495e65c1330b3f3de6eee9aceace456aee9f859
KAWOOYA RAYMOND <kawooyaraymond40@gmail.com>
2025-10-24 19:59:37 +0000

    remove it again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import SidebarNav from "@/components/sidebar-nav";
 import Header from "@/components/header";
 import { FirebaseClientProvider } from "@/firebase/client-provider";
-import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "CleanSweep Inventory",
@@ -38,7 +37,6 @@ export default function RootLayout({
               </SidebarInset>
           </SidebarProvider>
         </FirebaseClientProvider>
-        <Analytics />
       </body>
     </html>
   );
